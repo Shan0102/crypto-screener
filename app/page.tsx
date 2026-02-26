3d2703143a95e7ae35b9de74ce444c5c54292f06
Shan0102 <ilyaqudin2005@gmail.com>
2026-02-27 00:19:27 +0300

    home page coinoverview header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,39 @@
+import Image from "next/image";
+
 interface PageProps {}
 
-const Page = ({}: PageProps) => {};
+const Page = ({}: PageProps) => {
+    return (
+        <main className="py-2 md:py-4 px-2 md:px-4 max-w-360 mx-auto">
+            <div className="flex flex-col lg:flex-row gap-2 md:gap-4">
+                <section className="space-y-2 md:space-y-4">
+                    <div className="py-2 md:py-4 px-2 md:px-4 bg-gray-800/70 rounded-2xl">
+                        <div className="flex gap-2">
+                            <div>
+                                <Image
+                                    alt="bitcoin"
+                                    src={
+                                        "https://assets.coingecko.com/coins/images/1/large/bitcoin.png"
+                                    }
+                                    width={48}
+                                    height={48}
+                                />
+                            </div>
+                            <div className="flex flex-col justify-between">
+                                <p className="text-gray-400 text-xs">Bitcoin/BTC</p>
+                                <h1 className="text-2xl">$89,283.00</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <p>Coin Overview</p>
+                    <p>Trending Coins</p>
+                </section>
+                <section>
+                    <p>Categories</p>
+                </section>
+            </div>
+        </main>
+    );
+};
 
 export default Page;
--- components/Header.tsx
+++ components/Header.tsx
@@ -9,25 +9,27 @@ const Header = () => {
 
     return (
         <header className="border-b">
-            <div className="py-2 md:py-4 px-2 md:px-4 mx-auto flex items-center justify-between">
-                <Link href="/">
-                    <Image src="/logo.svg" alt="cryptoPulse logo" width={40} height={40} />
-                </Link>
+            <div className="py-2 md:py-4 px-2 md:px-4 max-w-360 mx-auto">
+                <div className="flex items-center justify-between">
+                    <Link href="/">
+                        <Image src="/logo.svg" alt="cryptoPulse logo" width={48} height={48} />
+                    </Link>
 
-                <nav className="flex items-center gap-2 md:gap-4 font-mediu">
-                    <Link
-                        href={"/"}
-                        className={`max-sm:hidden ${pathname === "/" ? "text-white" : "text-gray-400"}`}
-                    >
-                        Home
-                    </Link>
-                    <Link
-                        href={"/coins"}
-                        className={`${pathname === "/coins" ? "text-white" : "text-gray-400"}`}
-                    >
-                        All Coins
-                    </Link>
-                </nav>
+                    <nav className="flex items-center gap-2 md:gap-4 font-mediu">
+                        <Link
+                            href={"/"}
+                            className={`max-sm:hidden ${pathname === "/" ? "text-white" : "text-gray-400"}`}
+                        >
+                            Home
+                        </Link>
+                        <Link
+                            href={"/coins"}
+                            className={`${pathname === "/coins" ? "text-white" : "text-gray-400"}`}
+                        >
+                            All Coins
+                        </Link>
+                    </nav>
+                </div>
             </div>
         </header>
     );
