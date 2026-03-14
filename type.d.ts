afaff9bdd44d8f87d38900b3d752451c3357ad24
Shan0102 <ilyaqudin2005@gmail.com>
2026-03-14 23:02:14 +0300

    update coindeteil type

--- type.d.ts
+++ type.d.ts
@@ -2,6 +2,22 @@ interface CoinDetailsData {
     id: string;
     symbol: string;
     name: string;
+    links: {
+        homepage: string;
+    };
+    image: {
+        thumb: string;
+        small: string;
+        large: string;
+    };
+    market_data: {
+        current_price: {
+            usd: number;
+        };
+        market_cap_change_percentage_24h_in_currency: {
+            usd: number;
+        };
+    };
 }
 
 interface CoinData {
